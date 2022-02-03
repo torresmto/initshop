bf0c9b8f937e99bddb630928ee04fb951e8f44a5
torresmto <98768581+torresmto@users.noreply.github.com>
2022-02-03 22:10:41 +0100

    #Gestion de la page panier

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Cart} from '../models/cart';
 import {Products} from '../models/products';
 
--- src/app/shop/cart/cart.component.ts
+++ src/app/shop/cart/cart.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {CartService} from '../../services/cart.service';
+import {Cart} from '../../models/cart';
+import {environment} from '../../../environments/environment';
+import {Products} from '../../models/products';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  /* on déclare les attributs de classe */
+  cart: Cart[] = [];
+  prefUrlImage = `${environment.prefUrlImage}`;
 
-  constructor() { }
+  /* on injecte le service "cart" */
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.cart = this.cartService.cart;
+  }
+
+  /**
+   * Méthode permettant d'ajouter un nouveau produit au panier
+   * via le service cartService
+   * @param product Products
+   */
+  addProduct(product: Products): void {
+    this.cartService.addProductsToCart(product);
+  }
+
+  /**
+   * Méthode permettant de supprimer un produit du panier
+   * via le service cartService
+   * @param product Products
+   */
+  deleteProduct(product: Products): void {
+    this.cartService.deleteFromCart(product);
   }
 
 }
